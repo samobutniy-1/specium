8735f6658f5a25a4a91635e043886ff003c6667e
webOwl <nikitaterentev916@gmail.com>
2026-05-16 11:01:25 +0200

    feat: configure app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { AnimalClass } from './features/animal-class/animal-class';
+import { AnimalDetail } from './features/animal-detail/animal-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', title: 'Specium', component: Home },
+  { path: 'class/:id', title: 'Specium', component: AnimalClass },
+  { path: 'animal/:id', title: 'Specium', component: AnimalDetail },
+  { path: '**', redirectTo: '' },
+];
