3496c9ed795f7735c1d4b2d50da7658b4e81ac64
webOwl <nikitaterentev916@gmail.com>
2026-05-16 11:18:51 +0200

    feat: add Animal and AnimalClass interfaces

---
+++ src/app/shared/models/animal.model.ts
@@ -0,0 +1,23 @@
+export interface Animal {
+  id: string;
+  classId: string;
+  name: string;
+  scientificName: string;
+  order: string;
+  habitat: string[];
+  size: { min: number; max: number; unit: string };
+  weight: { min: number; max: number; unit: string };
+  lifespan: { min: number; max: number };
+  diet: string;
+  conservationStatus: string;
+  shortDescription: string;
+  fullDescription: string;
+  imageUrl: string;
+}
+
+export interface AnimalClass {
+  id: string;
+  name: string;
+  description: string;
+  imageUrl: string;
+}
