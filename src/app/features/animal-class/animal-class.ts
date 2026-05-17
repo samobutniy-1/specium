f626f8bb19b7c9fa6a9293c5545cbc55e0a0b9f0
webOwl <nikitaterentev916@gmail.com>
2026-05-17 18:42:01 +0200

    feat: implement animal-class component with filtered animals list

--- src/app/features/animal-class/animal-class.ts
+++ src/app/features/animal-class/animal-class.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AnimalService } from '../../core/services/animal';
 
 @Component({
   selector: 'app-animal-class',
@@ -6,4 +8,10 @@ import { Component } from '@angular/core';
   templateUrl: './animal-class.html',
   styleUrl: './animal-class.css',
 })
-export class AnimalClass {}
+export class AnimalClass {
+  route = inject(ActivatedRoute);
+  classId = this.route.snapshot.paramMap.get('id');
+  animalService = inject(AnimalService);
+  animals = this.animalService.getAnimalsByClass(this.classId!);
+  currentClass = this.animalService.getClasses().find((c) => c.id === this.classId);
+}
