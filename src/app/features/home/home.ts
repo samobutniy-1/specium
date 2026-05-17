f95c56e3fe29c2c1c74049370311e2ae9c2a2362
webOwl <nikitaterentev916@gmail.com>
2026-05-17 18:08:29 +0200

    feat: add navigation to animal class page

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { AnimalService } from '../../core/services/animal';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
